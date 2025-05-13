155e7c37a62259b2d964ced0726b4760eaa247c9
Anupamshraddha <anupamshraddha434@gmail.com>
2025-05-13 16:42:57 +0530

    Delete SmartSprint--QuizApp-main/SmartSprint--QuizApp-main directory

--- SmartSprint--QuizApp-main/SmartSprint--QuizApp-main/smartsprint/quiz_app1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('reviews/', views.reviews, name='reviews'),
-    path('external-reviews/', views.flask_reviews_view, name='external_reviews'),
-]
